3aca5fb41a0c56ff11b055a7531a5c7104aafef2
Shubham Nigam <shubham@Shubhams-MBP.local.meter>
2024-04-07 13:50:54 -0700

    keys

--- src/assets/Config/appConfig.js
+++ src/assets/Config/appConfig.js
@@ -1,8 +1,8 @@
 export const mainConfig = {
     QUEST_BASE_URL : "https://api.questprotocol.xyz/",
-    QUEST_ENTITY_ID: "",
-    QUEST_API_KEY: "",
-    CALENDLY_LINK: "",
+    QUEST_ENTITY_ID: "e-6358b0b5-d8ce-4d8f-b0b1-77f4889ffa45",
+    QUEST_API_KEY: "k-02dc58bf-8d9c-4cea-b8fd-836731e397a4",
+    CALENDLY_LINK: "https://calendly.com/shubham-quest/chat",
     GOOGLE_REDIRECT_URI: "",
     GOOGLE_CLIENT_ID: "",
 }
