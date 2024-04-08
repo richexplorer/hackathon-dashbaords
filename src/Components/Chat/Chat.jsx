d023326ca1b9886b14f2e5d214f624b254bf5b7b
debparna pratiher <dpratiher@gmail.com>
2024-04-07 17:15:08 -0700

    Deb2

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -2,6 +2,9 @@ import { useContext } from "react";
 import { mainConfig } from "../../assets/Config/appConfig";
 import { ThemeContext } from "../Common/AppContext";
 import Siriwave from "react-siriwave";
+import microphone from "../../assets/Images/microphone.svg";
+import end from "../../assets/Images/end.svg";
+import play from "../../assets/Images/play.svg";
 import "./Chat.css"
 import { zenithFunctions } from "../../assets/Config/zenithFunctions";
 
@@ -14,24 +17,34 @@ export default function Chat() {
                Ask Zenith Anything!
                <br />
                 <div className="flex-col justify-start items-center gap-1.5 flex">
-                    <div className=" text-center text-neutral-500 text-sm font-normal font-['Figtree'] leading-tight">
-                      <div className="Ball" 
-                        onClick={() => {
-                          zenithFunctions.toggleSession();
-                        }}
-                      />
-                    {/*  <div style={{zIndex: 1, position: relative}}>
-                        <Siriwave
-                          color="#3f51b5"
-                          speed="0.025"
-                          frequency= "5"
-                          amplitude="0.6"
-                          autostart= "true"
-                        />
-                      </div> */}
-                    </div>
-                </div>
 
+                    <div className=" text-center text-neutral-500 text-sm font-normal font-['Figtree'] leading-tight">
+                        <div style={{zIndex:1, position: 'absolute', top:0, left:0, paddingTop: '100px'}}>
+                          <div style = {{fontSize:"40px", color:"gray", paddingBottom:"30px", fontWeight: '3'}}> Let Zenith Help You ✨ </div>
+                          <div className="Ball"
+                            onClick={() => {
+                              zenithFunctions.toggleSession();
+                            }}
+                           />
+                        </div>
+                        <div style={{zIndex:2, position: 'absolute',  top:3, left:0, paddingTop: '385px'}}>
+                            <Siriwave
+                              color="#3f51b5"
+                              speed="0.025"
+                              frequency= "5"
+                              amplitude="0.6"
+                              autostart= "true"
+                            />
+                          </div>
+                    </div>
+
+                </div>
+                <div style={{display:"flex", paddingTop:"1000px",  paddingLeft:"100px"}}>
+                            <img src={microphone}  style={{paddingLeft:"9px"}}/>
+                            <img src={play}  style={{paddingLeft:"9px"}}/>
+                            <img src={end}  style={{paddingLeft:"22px"}}/>
+
+                </div>
             </div>
         </div>
     );
